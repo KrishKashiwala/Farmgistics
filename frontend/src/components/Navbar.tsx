49854e945af86ebd3bd2b77455c72cf23c041b06
krish kashiwala <krishkashiwala@gmail.com>
2021-07-24 00:08:05 +0530

    nav ready for desktop view

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@material-ui/core';
+import { Link, Avatar } from '@material-ui/core';
 import farmgistic_logo from './assests/farmgistic_logo.png';
 import './componentsCss/homepage.css';
 const Navbar = () => {
@@ -29,6 +29,23 @@ const Navbar = () => {
                 <a href="google.com" className="nav_a">
                     Contact Us
                 </a>
+                <a href="google.com" className="nav_a">
+                    About Us
+                </a>
+            </div>
+            <div className="nav_icon">
+                <div className="input-group rounded search">
+                    <input
+                        type="search"
+                        className="form-control rounded"
+                        placeholder="search query"
+                    />
+                    <i className="fa fa-search font"></i>
+                    {/* <i className="fas fa-shopping-bag"></i> */}
+                </div>
+                <i className="fas fa-shopping-bag font"></i>
+                <i className="fa fa-cog font"></i>
+                <Avatar alt="profile_pic" src={farmgistic_logo} />
             </div>
         </div>
     );
