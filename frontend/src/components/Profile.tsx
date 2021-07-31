4b9a5f88169d50f26cfc760494464b2792f772f2
krish kashiwala <krishkashiwala@gmail.com>
2021-07-31 08:04:25 +0530

    .

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,4 +1,34 @@
+import { Avatar, makeStyles, createStyles, Theme } from '@material-ui/core';
+import farmgistic_logo from './assests/farmgistic_logo.png';
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            '& > *': {
+                margin: theme.spacing(1)
+            }
+        },
+        small: {
+            width: theme.spacing(3),
+            height: theme.spacing(3)
+        },
+        large: {
+            width: '10em',
+            height: '10em'
+        }
+    })
+);
 const Profile = () => {
-    return <div>It's me </div>;
+    const classes = useStyles();
+    return (
+        <div className="root">
+            <Avatar
+                src={farmgistic_logo}
+                alt="profile_pic"
+                className={classes.large}
+            ></Avatar>
+            <h1>hei</h1>
+        </div>
+    );
 };
 export default Profile;
