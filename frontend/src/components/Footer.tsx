422a6d25fd302a978d7df9e86e3ba130abab3168
Akash Zalavadiya <akashzalavadiya1128@gmail.com>
2021-09-06 14:03:42 +0530

    Updated

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,6 @@ import PhoneInTalkOutlinedIcon from '@material-ui/icons/PhoneInTalkOutlined';
 import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
 
 
-
 const Footer = () => {
     return (
         <div>
